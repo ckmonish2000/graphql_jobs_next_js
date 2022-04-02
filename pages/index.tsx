8ccf2c079a72ac0566d985388f3e84b31e3b8bfb
monish karunakaran <ckmonish2000@gmail.com>
2022-04-02 23:06:25 +0530

    added sass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-
+import styles from "../styles/login.module.scss"
 
 export default function index() {
   return (
-    <div>indexsss</div>
+    <div className={styles.names}>indexsss</div>
   )
 }
