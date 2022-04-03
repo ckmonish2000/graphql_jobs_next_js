c19f05be903d82f8fb6810143430d9089990d4da
monish karunakaran <ckmonish2000@gmail.com>
2022-04-04 03:07:39 +0530

    progress in signup init

--- pages/Signup.tsx
+++ pages/Signup.tsx
@@ -4,14 +4,38 @@ import styles from "@styles/login.module.scss"
 import img from "@assets/letsstart.jpg"
 import LoginSignupLayout from "@layouts/LoginSignupLayout";
 import {Company, Personal, Request} from "@components/Signup_Mods"
+import {BiArrowBack} from "react-icons/bi"
+import {BsCircleFill} from "react-icons/bs"
 
 export default function Signup() {
-  const [active, setactive] = useState("request")
+  const [active, setactive] = useState("personal")
 
   return (
    <LoginSignupLayout img={img}>
         <React.Fragment>
-      
+
+        <div className={styles.signup_progress_root}>
+        <BiArrowBack className={styles.icon}/>
+
+      <div className={styles.signup_progress}>
+
+        <div className={styles.container}>
+          <BsCircleFill className={styles.ball}/>
+          <span>Personal</span>
+        </div>
+
+        <div className={styles.container}>
+          <BsCircleFill className={styles.ball}/>
+          <span>Company</span>
+        </div>
+
+        <div className={styles.container}>
+          <BsCircleFill className={styles.ball}/>
+          <span>Complete</span>
+        </div>
+      </div>
+
+        </div>
 
         <div className={styles.signup_container}>
         {active==="personal" && <Personal/>}
