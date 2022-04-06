1d8077796405cc1862d07e02ed2ac2bb54e2aa83
monish karunakaran <ckmonish2000@gmail.com>
2022-04-07 00:35:28 +0530

    navbar component and general layout

---
+++ pages/AddClients.tsx
@@ -0,0 +1,12 @@
+import GeneralLayout from '@layouts/GeneralLayout'
+import React from 'react'
+
+export default function AddClients() {
+  return (
+    <GeneralLayout
+    children={
+      <h1>AddClients</h1>
+    }
+    />
+  )
+}
---
+++ src/layouts/GeneralLayout.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import styles from "@styles/login.module.scss"
+import Layouts from "@layouts/layouts"
+import Navbar from "@components/navbar/Navbar"
+
+export default function GeneralLayout({children}:any) {
+  return (
+    <Layouts type="">
+    <React.Fragment>
+      {/* <div>ksdjshhuh</div> */}
+    <Navbar/>
+
+      <div className={styles.third_div}>
+      {children}
+      </div>
+      </React.Fragment>
+    </Layouts>
+  )
+}
